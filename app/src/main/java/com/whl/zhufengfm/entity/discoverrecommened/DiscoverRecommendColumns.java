43dae395cdd12018fd629068203c9f480f0a2367
zhaoQiang <229457269@qq.com>
2015-10-21 12:13:29 +0800

    发现中小编推荐完成

---
+++ app/src/main/java/com/whl/zhufengfm/entity/discoverrecommened/DiscoverRecommendColumns.java
@@ -0,0 +1,11 @@
+package com.whl.zhufengfm.entity.discoverrecommened;
+
+/**
+ * Project_name : ZhuFengFM
+ * Author : zhaoQiang
+ * Date : 2015/10/21,11:35
+ * Email : zhaoq_hero@163.com
+ */
+public class DiscoverRecommendColumns extends DiscoverRecommendItem{
+    
+}
