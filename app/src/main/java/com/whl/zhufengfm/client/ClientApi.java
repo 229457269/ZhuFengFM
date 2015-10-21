256dbe1ba18a769f17a2450f1b198b09a1ffa2c8
zhaoQiang <229457269@qq.com>
2015-10-21 19:26:56 +0800

    发现中小编推荐完成01

--- app/src/main/java/com/whl/zhufengfm/client/ClientApi.java
+++ app/src/main/java/com/whl/zhufengfm/client/ClientApi.java
@@ -1,5 +1,7 @@
 package com.whl.zhufengfm.client;
 
+import android.util.Log;
+
 import com.whl.zhufengfm.utils.HttpTools;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,23 +21,71 @@ public final class ClientApi {
      */
     private static final String API_POINT = "http://mobile.ximalaya.com";
 
-    private ClientApi(){}
+    private ClientApi() {
+    }
 
     //-----------------------
     //接口 12 ：获取发现的分类，
 
     /**
-     *  地址：
-     * 	/mobile/discovery/v1/categories
-     * 	        ?device=android&picVersion=10&scale=2
-     * @return
+     * 地址：
+     * http://mobile.ximalaya.com/mobile/discovery/v1/categories?device=android&picVersion=10&scale=2
+     *
+     * @return JSONOBJECT
      */
-    public static JSONObject getDiscoverCategories(){
+    public static JSONObject getDiscoverCategories() {
         JSONObject ret = null;
 
-        byte[] data = HttpTools.doGet(API_POINT+"/mobile/discovery/v1/categories" +
-                "?device=android&picVersion=10&scale=2");
+        byte[] data = HttpTools.doGet(API_POINT + "/mobile/discovery/v1/categories?device=android&picVersion=10&scale=2");
 
+        if (data != null) {
+            try {
+
+                String str = new String(data, "utf-8");
+
+                Log.d("debug11","str="+str);
+
+
+                ret = new JSONObject(str);
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return ret;
+    }
+
+    //-----------------------
+    /**
+     * 接口11：用于  获取发现部分  推荐列表的内容
+     * /mobile/discovery/v1/recommends?channel=and-f6&device=android&
+     * includeActivity=true&includeSpecial=true&scale=2&version=4.1.7.1
+     * @param channel  软件发布渠道
+     * @param includeActivity   是否包含活动
+     * @param includeSpecial   是否包含活动清单
+     * @return
+     */
+    public static JSONObject getDiscoverRecommed(
+            String channel, boolean includeActivity, boolean includeSpecial) {
+
+        JSONObject ret = null;
+
+        if(channel == null){
+            channel = "and-f6";
+        }
+
+        String url = API_POINT +
+                "/mobile/discovery/v1/recommends"+
+                "?channel="+channel
+                +"&device=android"
+                +"&includeActivity=" +includeActivity
+                +"&includeSpecial=" + includeSpecial
+                +"&scale=2"
+                +"&version=4.1.7.1";
+
+        byte[] data = HttpTools.doGet(url);
         if (data != null) {
             try {
                 String str = new String(data,"UTF-8");
@@ -49,12 +99,12 @@ public final class ClientApi {
         }
         return ret;
     }
-    //-----------------------
 
 
     //-----------------------
     //-----------------------
     //-----------------------
     //-----------------------
+
 
 }
